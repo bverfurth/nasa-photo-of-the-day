b4b92b087fff93990664238c76bb7be9876d7f24
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-14 20:41:23 -0400

    Edited layout and background

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,40 @@
 import React from "react";
-
+import styled from "styled-components";
+import stars from "../Assets/stars.gif";
 export default function Header(props) {
   const { data } = props;
 
+  const HeaderContainer = styled.div`
+    background-image: url(${stars});
+    div {
+      color: black;
+    }
+    h1 {
+      background-image: url(${stars});
+      color: white;
+    }
+    a {
+      color: white;
+    }
+    p {
+      color: white;
+    }
+  `;
+
   if (!data) return <h3>Loading...</h3>;
   return (
-    <>
+    <HeaderContainer>
       <h1>
         Astronomy Picture of the Day <span>🚀</span>
       </h1>
+      <a>Click Here!! ---{">"} </a>
       <a href="astropix.html">Discover the cosmos! </a>
-      Each day a different image or photograph of our fascinating universe is
-      featured, along with a brief explanation written by a professional
-      astronomer.
+      <p>
+        Each day a different image or photograph of our fascinating universe is
+        featured, along with a brief explanation written by a professional
+        astronomer.
+      </p>
       <p>{data.date}</p>
-    </>
+    </HeaderContainer>
   );
 }
--- src/Components/Media.js
+++ src/Components/Media.js
@@ -1,22 +1,61 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import styled from "styled-components";
+import stars from "../Assets/stars.gif";
+
+const MediaContainer = styled.div`
+  background-image: url(${stars});
+  h1 {
+    color: white;
+    background-color: gray;
+    &:hover {
+      transform: scale(2);
+      transition: all 0.05s ease-in-out;
+    }
+    transition: all 2s ease-in-out;
+  }
+  .video {
+    margin: 0 auto;
+  }
+  img {
+    width: 30em;
+    height: 30em;
+    margin: 0 auto;
+    &:hover {
+      transform: scale(1.5);
+      transition: all 5s ease-in-out;
+    }
+    transition: all 0.05s ease-in-out;
+  }
+  transition: all 2s ease-in-out;
+  p {
+    color: white;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+  }
+  h4 {
+    color: white;
+  }
+`;
 
 export default function Media(props) {
   const { data } = props;
 
   if (!data) return <h3>Loading...</h3>;
   return (
-    <>
-      <ReactPlayer url={data.url} />
+    <MediaContainer>
+      <ReactPlayer url={data.url} className="video" />
       <h4>{data.title}</h4>
       <p>
         <b>Image Credit & Copyright: </b>
+        <img src="https://images-assets.nasa.gov/image/PIA18033/PIA18033~orig.jpg"></img>
         {data.copyright}
       </p>
       <p>
         <b>Explanation: </b>
         {data.explanation}
       </p>
-    </>
+    </MediaContainer>
   );
 }
